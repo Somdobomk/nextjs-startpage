88727055d8b64ee81a996d240ac564e0e5ce4946
somdobomk <somdobomk@gmail.com>
2022-01-27 12:16:44 -0600

    add urls and theme to start page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        victormono: ['Victor Mono', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
